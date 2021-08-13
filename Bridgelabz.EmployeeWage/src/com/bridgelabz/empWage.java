ebce6d578e9279d853ce95ab8e288210832792ce
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-13 16:51:06 +0530

    Refactor:Compute the EmpWage Using Oops

--- Bridgelabz.EmployeeWage/src/com/bridgelabz/empWage.java
+++ Bridgelabz.EmployeeWage/src/com/bridgelabz/empWage.java
@@ -1,18 +1,29 @@
+package com.bridgelabz;
+
+/**
+ * empWage is a application that implements
+ * employee wage detatils
+ * simuklates the totalworking Wage.
+ * Prints the output on screen
+ * @author Sunil
+ * @version 16.0
+ * @since 13/08/2021
+ */
 public class empWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int WAGE_PER_HR = 20;
-     public static final int WORKING_DAYS = 20;
+     public static final int NUM_WORKING_DAYS = 20;
      public static final int MAX_WORKING_HRS = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+    public static int computEmpWage(){
+        //variable decleration
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays =0;
 
         // Computation
-        while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays<WORKING_DAYS) {
+        while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays< NUM_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -28,12 +39,14 @@ public class empWage {
             totalEmpHrs += empHrs;
             System.out.println("Day : " + totalWorkingDays + " Employee Hrs: " +empHrs);
         }
-        System.out.println("Total Hours worked =" + totalEmpHrs);
-        System.out.println("Number of days worked" +totalWorkingDays);
         int totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-        System.out.println("Total Employee Salary= " + totalEmpWage);
+        System.out.println("Total Employee Wage = " + totalEmpWage);
+        return  totalEmpWage;
     }
 
+    public static void main(String[] args) {
+        computEmpWage();
+    }
 
 
 }
---
+++ LineComaprision/src/Bridgelabz/LineComparision.java
@@ -0,0 +1,2 @@
+package Bridgelabz;public class LineComparision {
+}
